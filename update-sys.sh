b9db88a051d6365fcf4f2d811424f0a63f6eed89
moxi-git <207268292+moxi-git@users.noreply.github.com>
2025-10-12 14:48:22 +0200

    added reboot prompt to update-sys script Signed-off-by: moxi-git <207268292+moxi-git@users.noreply.github.com>

--- update-sys.sh
+++ update-sys.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+# update sys
 sudo pacman -Sy && sudo pacman -Syu
+
+# reboot prompt
+echo "do you want to reboot y/n"
+read input
+
+if [ "$input" = "y" ]; then
+  sudo reboot
+else
+  echo "not right input"
+fi
